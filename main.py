4fbae32c5a92d3fc6927834ab1076fd277bb7349
minhosong88 <hominsong@naver.com>
2024-07-07 15:25:50 -0700

    Oauthorize
    implemented a simple O authorize

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Body, Form
+from fastapi.responses import HTMLResponse
 import logging
-from typing import Optional
+from typing import Optional, Any, Dict
 from pydantic import BaseModel, Field
 
 app = FastAPI(
@@ -8,7 +9,7 @@ app = FastAPI(
     description="Get a real quote said by Nicolacus Maximus the Third himself",
     servers=[
         {   # Make sure you don't have a slash at the end of the url
-            "url": "https://blackberry-remain-golden-non.trycloudflare.com",
+            "url": "https://dance-increased-julian-slots.trycloudflare.com",
         }
     ]
 )
@@ -39,4 +40,36 @@ logging.basicConfig(level=logging.INFO)
 )
 def get_quote(request: Request):
     logging.info(f"Endpoint {request.url.path} was called")
+    print(request.headers)
     return {"quote": "Life is short so enjoy whatever you can eat", "year": 1950}
+
+
+user_token_db = {
+    "ABCDE": "minho"
+}
+
+
+@app.get("/authorize",
+         response_class=HTMLResponse,
+         include_in_schema=False,
+         )
+def handle_authorize(client_id: str, redirect_uri: str, state: str):
+    return f"""
+    <html>
+        <head>
+            <title>Nicolacus Maximus Log In</title>
+        </head>
+        <body>
+            <h1> Log into Nicolacus Maximus</h1>
+            <a href="{redirect_uri}?code=ABCDE&state={state}">Authorize NicolacusMaximus GPT</a>
+        </body>
+    </html>
+    """
+
+
+@app.post("/token",
+          include_in_schema=False,)
+def handle_token(code=Form(...)):
+    return {
+        "access_token": user_token_db[code]
+    }
